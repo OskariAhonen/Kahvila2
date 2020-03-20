0bd1792a52ae64b306b181a0fad839ed5c990aab
OskariAhonen <oskari.m.ahonen@eduespoo.fi>
2020-03-20 09:35:13 +0200

    pieniä muutoksia v 3

--- src/kahvila/Asiakas.java
+++ src/kahvila/Asiakas.java
@@ -18,50 +18,53 @@ public class Asiakas {
         return this.bonus;
     }
     public void otaRaha(String tuote, int maksu) {
+       while (true) {
         Scanner ok = new Scanner(System.in);
-       if (bonus < 10) {
-           System.out.print("Oletko varma että haluat ostaa tuotteen " + tuote + ": ");
-           String vahvistus = ok.nextLine();
+        if (bonus < 10) {
+            System.out.print("Oletko varma että haluat ostaa tuotteen " + tuote + " k/e: ");
+            String vahvistus = ok.nextLine();
 
-           if (vahvistus.equals("kyllä")) {
-               if (raha < maksu) {
-                       System.out.println("Tuotetta ei voi ostaa, liian vähän rahaa");
-                   } else {
-                      this.raha = raha - maksu;
-                       System.out.println("tuote ostetiin.");
-                       this.bonus = this.bonus + 1;
-                   }
-               }
+            if (vahvistus.equals("k")) {
+                if (raha < maksu) {
+                    System.out.println("Tuotetta ei voi ostaa, liian vähän rahaa");
+                    break;
+                } else {
+                    this.raha = raha - maksu;
+                    System.out.println("tuote ostetiin.");
+                    this.bonus = this.bonus + 1;
+                    break;
+                }
+            } else if (vahvistus.equals("e")) {
+                System.out.println("Tuotetta ei osteta");
+                break;
+            } else {
+                System.out.println("Anna kelvollinen vastaus");
+            }
+        } else if (bonus == 10) {
+            System.out.print("Haluatko käyttää 10 bonuspistettä saadaksesi tuotteen ilmaiseksi k/e: ");
+            String b = ok.nextLine();
+            if (b.equals("k")) {
+                System.out.println("Tuote ostetiin!");
+                bonus = bonus - 10;
+                break;
+            } else if (b.equals("e")) {
+                System.out.print("Oletko varma että haluat ostaa tuotteen rahoillasi k/e: " + tuote + ": ");
+                String vahvistus = ok.nextLine();
+                if (vahvistus.equals("k")) {
+                    if (raha < maksu) {
+                        System.out.println("Tuotetta ei voi ostaa, liian vähän rahaa");
+                        break;
+                    } else {
+                        raha = raha - maksu;
+                        System.out.println("Tuote ostettiin!");
+                        break;
+                    }
+                }
 
-           else {
-               System.out.println("Tuotetta ei osteta");
-           }
-       }
-       else if (bonus == 10) {
-           System.out.print("Haluatko käyttää 10 bonuspistettä saadaksesi tuotteen ilmaiseksi: ");
-           String b = ok.nextLine();
-           if (b.equals("kyllä")) {
-               System.out.println("Tuote ostetiin!");
-               bonus = bonus - 10;
-           }
-           else if (b.equals("ei")) {
-               System.out.print("Oletko varma että haluat ostaa tuotteen rahoillasi: " + tuote + ": ");
-               String vahvistus = ok.nextLine();
-               if (vahvistus.equals("kyllä")) {
-                   if (raha < maksu) {
-                       System.out.println("Tuotetta ei voi ostaa, liian vähän rahaa");
-                   } else {
-                       raha = raha - maksu;
-                       System.out.println("Tuote ostettiin!");
-                   }
-               }
-               if (vahvistus.equals("ei")) {
-                   System.out.println("Tuotetta ei osteta!");
-               }
 
-           }
-       }
-
+            }
+        }
+    }
     }
 
 
